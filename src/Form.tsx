04251538b93ff18acac4429b8e2d83f5baa86f39
Eivind Samuelsen <eivind.samuelsen@gmail.com>
2024-10-17 16:45:37 +0200

    Separated value parsing for form elements out to a separate file

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,6 +1,7 @@
 import { type FormEvent, type ReactElement, type ReactNode, useCallback } from 'react';
 import { Text, TextProps } from './components/Text';
 import { Choice, ChoiceProps } from './components/Choice';
+import { parseElementValue } from './components/formValues';
 import './form.css';
 
 type Errors<T extends object> = { [K in keyof T & string]?: string };
@@ -19,37 +20,18 @@ interface Form<T extends object> extends FormComponent<T> {
 
 type Validator<T extends object> = (raw: { [K in string]?: unknown }) => { data: T | null, errors: Errors<T> };
 
-const assumeValid: Validator<any> = (raw) => ({ data: raw, errors: {}});
+const assumeValid: Validator<any> = (raw) => ({data: raw, errors: {}});
 
 export function createForm<T extends object>(validate: Validator<T> = assumeValid): Form<T> {
     const Form: Partial<Form<T>> & FormComponent<T> = ({onSubmit, children}: FormProps<T>) => {
         const handleSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
             event.preventDefault();
             // TODO: more intelligent formdata handling for eg numbers
-            const raw = Object.fromEntries(new FormData(event.currentTarget)) as Record<string, any>;
+            const raw: Record<string, any> = {};
             for (const input of event.currentTarget.elements) {
-                if (input instanceof HTMLInputElement) {
-                    if (input.type === 'checkbox') {
-                        if (!Array.isArray(raw[input.name])) {
-                            raw[input.name] = [];
-                        }
-                        if (input.checked) {
-                            raw[input.name].push(input.value);
-                        }
-                    }
-                }
-                if (input instanceof HTMLSelectElement) {
-                    if (input.multiple) {
-                        raw[input.name] = [];
-                        for (const option of input.querySelectorAll('option')) {
-                            if (option.selected) {
-                                raw[input.name].push(option.value);
-                            }
-                        }
-                    }
-                }
-            }
-            const { data, errors } = validate(raw);
+                parseElementValue(input, raw);
+           }
+            const {data, errors} = validate(raw);
             if (data) {
                 onSubmit(data, errors);
             }
---
+++ src/components/formValues.ts
@@ -0,0 +1,44 @@
+
+
+const PARSER_FOR = new Map<unknown, (element: unknown, raw: Record<string, any>) => void>()
+const PARSER_FOR_INPUT = new Map<string, (element: HTMLInputElement, raw: Record<string, any>) => void>()
+
+
+PARSER_FOR.set(HTMLSelectElement, (element: HTMLSelectElement, raw) => {
+    if (element.multiple) {
+        const values: string[] = [];
+        raw[element.name] = values;
+        for (const option of element.querySelectorAll('option')) {
+            if (option.selected) values.push(option.value);
+        }
+    } else {
+        raw[element.name] = element.value;
+    }
+});
+PARSER_FOR.set(HTMLInputElement, (element: HTMLInputElement, raw) => {
+    const typeSpecificParser = PARSER_FOR_INPUT.get(element.type);
+    if (typeSpecificParser) {
+        typeSpecificParser(element, raw);
+    } else {
+        raw[element.type] = element.value;
+    }
+});
+PARSER_FOR_INPUT.set('checkbox', (element, raw) => {
+    if (element.value) {
+        let values = raw[element.name];
+        if (!Array.isArray(values)) {
+            values = [];
+            raw[element.name] = values;
+        }
+        if (element.checked) {
+            values.push(element.value);
+        }
+    } else {
+        raw[element.value] = element.checked;
+    }
+})
+
+
+export function parseElementValue(element: Element, raw: Record<string, any>) {
+    PARSER_FOR.get(element.constructor)?.(element, raw);
+}
